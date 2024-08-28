18e1bddd8ee3bed227f3478c939f8389b523eefa
Jakeer <Jakeer.com>
2024-08-28 11:02:47 +0530

    conversation file commited

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Author - Jakeer
+Team - Devops
+echo " hi Jakeer how are you "
+echo " hello rajesh iam good "
+echo " whats going on Jakeer "
+echo " nothing rajesh plan to go to trip "
