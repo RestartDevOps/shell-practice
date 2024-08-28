b78e45eabe215735b038fb2401c509547f167ceb
Jakeer <Jakeer.com>
2024-08-28 11:13:00 +0530

    added few lines in variables file commited

--- variables.sh
+++ variables.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-Author - Jakeer
-Team - Devops
-PERSON1=Jakeer
-PERSON2=Rajesh
+#Author - Jakeer
+#Team - Devops
+#PERSON1=Jakeer
+#PERSON2=Rajesh
+#echo " hi $PERSON1 how are you "
+#echo " hello $PERSON2 iam good "
+#echo " whats going on $PERSON1 "
+#echo " nothing  plan $PERSON2 to go to trip "
+read -p "please enter your 1st person name " PERSON1
+read -p " please enter your 2ndperson name " PERSON2
 echo " hi $PERSON1 how are you "
 echo " hello $PERSON2 iam good "
 echo " whats going on $PERSON1 "
