88cf9f8e2d2c1c91ae3650fee56d936775282881
Jakeer <Jakeer.com>
2024-08-28 11:54:23 +0530

    array file few lines added

--- 06-array.sh
+++ 06-array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Author - Jakeer
 #Team - Devops
-fruits="(apple banana guava mango)"
-echo " friuts are ${fruits[0]} "
-echo " friuts are ${fruits[1]} "
-echo " friuts are ${fruits[2]} "
-echo " friuts are ${fruits[3]} "
+fruits=("apple" "banana" "guava" "mango")
+echo " friuts are '${fruits[0]}' "
+echo " friuts are '${fruits[1]}' "
+echo " friuts are '${fruits[2]}' "
+echo " friuts are '${fruits[3]}' "
