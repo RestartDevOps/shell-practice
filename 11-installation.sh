c86d8cf87ee245ed0972ccdf2caa88a5dd4c41ca
Jakeer <Jakeer.com>
2024-08-28 12:50:03 +0530

    commits installation file

---
+++ 11-installation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Author - Jakeer
+#Team - Devops
+USERID=$(id -u)
+if [ $USERID -ne 0 ];then
+echo " you don't have root previlages to run this command please run with root privilages or add sudo before the command "
+else 
+echo " you have root previlages to run this command
