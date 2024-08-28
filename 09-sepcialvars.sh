82af5c18b6e1499406b0aef1f58f3138f0b0f1a2
Jakeer <Jakeer.com>
2024-08-28 12:36:54 +0530

    commits specialvars file

--- 09-sepcialvars.sh
+++ 09-sepcialvars.sh
@@ -2,7 +2,8 @@
 #Author - Jakeer
 #Team - Devops
 echo " all the variable are passed $@ "
-echo " $# "
-echo " $0 "
-echo " $PWD "
-echo " $HOME "
+echo " how many variables are passed $# "
+echo " to display file name $0 "
+echo " current working dicrectory is .. $PWD "
+echo " present home directory is .. $HOME "
+echo " process id $$ "
