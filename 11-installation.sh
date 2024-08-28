af7c5e3e256c41850f0572cc07e2f6444f79a84e
Jakeer <Jakeer.com>
2024-08-28 12:51:38 +0530

    commits installation file

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,5 +5,5 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ];then
 echo " you don't have root previlages to run this command please run with root privilages or add sudo before the command "
 else 
-echo " you have root previlages to run this command
+echo " you have root previlages to run this command "
 fi
