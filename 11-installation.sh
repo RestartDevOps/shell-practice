8947221731cf3f18500f3369cb5c5446049ef016
Jakeer <Jakeer.com>
2024-08-28 13:56:42 +0530

    commits installation file

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,6 +4,7 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ];then
 echo " you don't have root previlages to run this command please run with root privilages or add sudo before the command "
+exit 1
 else 
 echo " you have root previlages to run this command "
 fi
@@ -11,6 +12,11 @@ dnf list installed git
 if [ $? -ne 0 ];then
 echo " git is not installed "
 dnf install git -y
+if [ $? -ne 0 ];then
+echo " git installation fialed
+else 
+echo " git installation success "
+fi
 else 
 echo " git installed nothing to do "
 fi
