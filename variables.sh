9793140a61d075d8813f86cf94f98eb7e2b32c44
Jakeer <Jakeer.com>
2024-08-28 11:08:04 +0530

    variables file commited

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+Author - Jakeer
+Team - Devops
+PERSON1=Jakeer
+PERSON2=Rajesh
+echo " hi $PERSON1 how are you "
+echo " hello $PERSON2 iam good "
+echo " whats going on $PERSON1 "
+echo " nothing  plan $PERSON2 to go to trip "
