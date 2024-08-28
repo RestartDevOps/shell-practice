0d98682cc4607dda91854ae2267640f7d8374919
Jakeer <Jakeer.com>
2024-08-28 11:33:44 +0530

    variable file commited

---
+++ 04-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Author - Jakeer
+#Team - Devops
+#PERSON1=Jakeer
+#PERSON2=Rajesh
+echo " hi $1 how are you "
+echo " hello $2 iam good "
+echo " whats going on $1 "
+echo " nothing  plan $2 to go to trip "
+#read -p "please enter your 1st person name " $PERSON1
+#read -p " please enter your 2ndperson name " $PERSON2
+#echo " hi $PERSON1 how are you "
+#echo " hello $PERSON2 iam good "
+#echo " whats going on $PERSON1 "
+#echo " nothing  plan $PERSON2 to go to trip "
---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Author - Jakeer
+#Team - Devops
+read -p " please enter your user name " USERNAME
+read -s -p " please enter your passwd is " PASSWD
+echo " username is $USERNAME\n passwd is $PASSWD "
+
