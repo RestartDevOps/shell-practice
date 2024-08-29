4f308eac4b05ee763c5cfde75799386c70982aeb
Jakeer <Jakeer.com>
2024-08-29 16:39:22 +0530

    expence shell commits

--- 17-set.sh
+++ 17-set.sh
@@ -5,7 +5,7 @@ set -e #setting automatic error
 failure(){
     echo " failed at: $1:$2 "
 }
-trap 'failure "${LINENO}" "$(BASH_COMMAND)" ERR'
+trap 'failure "${LINENO}" "$BASH_COMMAND" ERR' #ERR is the error signal
 echo " Hello world success "
 echooo " Hello world failure "
 echo " Hello world after failure "
