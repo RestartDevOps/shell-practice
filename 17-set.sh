1407a13cd9a8a23272a965845ca6499b0d62f3ce
Jakeer <Jakeer.com>
2024-08-29 16:36:33 +0530

    expence shell commits

--- 17-set.sh
+++ 17-set.sh
@@ -2,6 +2,10 @@
 #Author - Jakeer
 #Team - DevOps
 set -e #setting automatic error 
+failure(){
+    echo " failed at: $1:$2 "
+}
+trap 'failure "${LINENO}" "$(BASH_COMMAND)" ERR'
 echo " Hello world success "
 echooo " Hello world failure "
 echo " Hello world after failure "
